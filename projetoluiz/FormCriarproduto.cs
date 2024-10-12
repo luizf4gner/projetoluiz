39871f5bd44f27406a376e64423f3891ddfacfbe
luizf4gner <luizfagnersilva43@gmail.com>
2024-10-11 21:19:41 -0300

    Add files via upload

---
+++ projetoluiz/FormCriarproduto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetoluiz
+{
+    public partial class FormCriarproduto : Form
+    {
+        public string nomeProduto { get { return this.textBoxNome.Text; } }
+        public string nomeFabricante { get { return this.textBoxFabricante.Text; } }
+        public float precoCompra { get { return (float)this.InputPrecoCompra.Value; } }
+        public float PrecoVenda { get { return (float)this.inputPrecoVenda.Value; } }
+        public FormCriarproduto()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
---
+++ projetoluiz/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoluiz
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [DisplayName("Produto")]
+        public string Nome { get; set; }
+        public string Fabricante { get; set; }
+        [DisplayName("Preço de compra")]
+        public float precoCompra { get; set; }
+        [DisplayName("Preço de venda")]
+        public float precoVenda { get; set; }
+    }
+
+}
